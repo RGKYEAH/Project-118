4974e82c80ab8cc6362610830dd07f753ffb4aed
RGKYEAH <106375824+RGKYEAH@users.noreply.github.com>
2022-11-16 18:49:02 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(250,250);
+    canvas.center();
+    background("darkviolet");
+}
+
+function clearCanvas() {
+    background("white");
+}
